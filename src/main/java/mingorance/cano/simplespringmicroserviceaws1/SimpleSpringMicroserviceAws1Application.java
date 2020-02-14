6587bdb184521787228045110e897c310584190a
Enrique Mingorance Cano <emingora@redhat.com>
2020-02-14 07:48:07 +0100

    Spring Initializr generated

---
+++ src/main/java/mingorance/cano/simplespringmicroserviceaws1/SimpleSpringMicroserviceAws1Application.java
@@ -0,0 +1,13 @@
+package mingorance.cano.simplespringmicroserviceaws1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringMicroserviceAws1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringMicroserviceAws1Application.class, args);
+	}
+
+}
---
+++ src/test/java/mingorance/cano/simplespringmicroserviceaws1/SimpleSpringMicroserviceAws1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mingorance.cano.simplespringmicroserviceaws1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringMicroserviceAws1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
